de05c7985f32c3fb72efd9669a75ea04098a2080
Goran Alković <goran.alkovic@hotmail.com>
2021-04-10 01:26:34 +0200

    add hero element

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,52 @@
-<script>
-	import Counter from '$lib/Counter.svelte';
-</script>
-
 <main>
-	<h1>Hello world!</h1>
-
-	<Counter />
-
-	<p>Visit <a class="text-blue-600 underline" href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
-	<p>Visit <a href="/recipes/example-markdown">the /recipes/example-markdown page</a> to see some markdown rendered by mdsvex.</p>
+	<section class="intro h-screen grid grid-cols-1 grid-rows-1 bg-black pl-10 md:pl-24 2xl:pl-96">
+		<picture>
+			<source media="(max-width: 767px)" srcset="./header-mobile.png" />
+			<source media="(max-width: 1800px)" srcset="./header-medium.png" />
+			<img
+				class="intro-image select-none pointer-events-none h-screen justify-self-end object-cover ml-auto mr-0"
+				src="./header-large.png"
+				alt="sliced strawberry and chocolate cake on white ceramic plate"
+			/>
+		</picture>
+		<div class="self-center flex flex-col select-none">
+			<span class="inline-block pl-1 text-xl md:text-2xl text-white -mb-2">Goc’s</span>
+			<h1 class="text-white font-display text-7xl sm:text-8xl font-bold">Recipe <br /> book</h1>
+		</div>
+		<span class="justify-self-end m-3 text-gray-700 hover:text-gray-200 transition-colors">
+			Photo by <a class="underline" target="_blank" href="https://unsplash.com/@sarahjulia">Sara Julie</a>
+		</span>
+		<button class="flex space-x-3 uppercase tracking-wider text-raspberry hover:text-white transition-colors self-end justify-self-start mb-20">
+			<svg
+				width="22"
+				height="22"
+				viewBox="0 0 22 22"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<circle cx="11" cy="11" r="10.5" stroke="currentColor" />
+				<path
+					d="M11 6.76923V15.2308M11 15.2308L15.2308 10.7511M11 15.2308L6.76923 10.7511"
+					stroke="currentColor"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				/>
+			</svg>
+			<span>Explore</span>
+		</button>
+	</section>
 </main>
 
 <style style lang="postcss">
-	main {
-		@apply text-center;
-		@apply p-4;
-		@apply mx-auto;
+	.intro > * {
+		grid-row: 1;
+		grid-column: 1;
 	}
 
-	h1 {
-		@apply text-red-600;
-		@apply uppercase;
-		@apply text-6xl;
-		@apply font-thin;
-		@apply leading-tight;
-		@apply my-16 mx-auto;
-		@apply max-w-xs;
-	}
-
-	p {
-		@apply max-w-xs;
-		@apply my-8 mx-auto;
-		@apply leading-snug;
-	}
-
-	@screen sm {
-		h1 {
-			@apply max-w-none;
-		}
-
-		p {
-			@apply max-w-none;
+	@screen md {
+		.intro,
+		.intro-image {
+			height: 80vmin;
 		}
 	}
 </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,28 +1,49 @@
-const { tailwindExtractor } = require("tailwindcss/lib/lib/purgeUnusedStyles");
+const { tailwindExtractor } = require('tailwindcss/lib/lib/purgeUnusedStyles');
+const colors = require('tailwindcss/colors')
 
 module.exports = {
-	mode: "aot",
+	mode: 'jit',
 	purge: {
-		content: [
-			"./src/**/*.{html,js,svelte,ts}",
-		],
+		content: ['./src/**/*.{html,js,svelte,ts}'],
 		options: {
 			defaultExtractor: (content) => [
 				// If this stops working, please open an issue at https://github.com/svelte-add/tailwindcss/issues rather than bothering Tailwind Labs about it
 				...tailwindExtractor(content),
 				// Match Svelte class: directives (https://github.com/tailwindlabs/tailwindcss/discussions/1731)
-				...[...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(([_match, group, ..._rest]) => group),
+				...[...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(
+					([group]) => group
+				)
 			],
-			keyframes: true,
-		},
+			keyframes: true
+		}
 	},
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: ['Karla', 'sans-serif'],
+			display: ['Caudex', 'serif']
+		},
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			black: colors.black,
+			white: colors.white,
+			gray: colors.warmGray,
+			raspberry: '#EF6373',
+			mint: {
+				DEFAULT: '#5E8225',
+				600: '#656B5C',
+				700: '#232C16',
+			},
+			muffin: {
+				DEFAULT: '#9F6145',
+				600: '#5D3F31',
+				100: '#FBF7F6',
+			},
+		},
+		extend: {}
 	},
 	variants: {
-		extend: {},
+		extend: {}
 	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
+	plugins: [require('@tailwindcss/typography')]
 };
