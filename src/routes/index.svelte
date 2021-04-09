11ec008e64076d8ca7517a447974f702b5a9f756
Goran Alković <goran.alkovic@hotmail.com>
2021-04-09 22:57:52 +0200

    add mdsvex

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,6 +8,7 @@
 	<Counter />
 
 	<p>Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
+	<p>Visit <a href="/example-markdown">the /example-markdown page</a> to see some markdown rendered by mdsvex.</p>
 </main>
 
 <style>
