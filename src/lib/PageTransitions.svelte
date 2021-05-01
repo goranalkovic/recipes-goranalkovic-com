1b405a2760932907fe4d1c500b3dc6f29d84aedc
Goran Alković <goran.alkovic@hotmail.com>
2021-05-01 19:59:52 +0200

    add simple page transitions

---
+++ src/lib/PageTransitions.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { slide, fade } from 'svelte/transition';
+	export let refresh = '';
+</script>
+
+{#key refresh}
+	<div in:slide={{ duration: 500, delay: 50 }} out:slide={{ duration: 500 }}>
+		<slot />
+	</div>
+{/key}
